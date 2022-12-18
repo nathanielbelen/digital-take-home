dac715ac5caa917ae972a7fe0e2d438e7aff9e2f
nathanielbelen <nathaniel.belen@gmail.com>
2022-12-17 18:31:50 -0800

    added a mask to content when dropdown is visible

--- src/components/Header/desktop/Dropdown.jsx
+++ src/components/Header/desktop/Dropdown.jsx
@@ -3,15 +3,18 @@ import PropTypes from 'prop-types';
 const Dropdown = ({ navigation, selectedIndex }) => {
   if (selectedIndex !== null) {
     return (
-      <div className="dropdown">
-        <ul className="dropdown-content">
-          {navigation[selectedIndex].next.map((destination, index) => (
-            <li className="links" key={index}>
-              <a href="/">{destination.name}</a>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <>
+        <div className="dropdown">
+          <ul className="dropdown-content">
+            {navigation[selectedIndex].next.map((destination, index) => (
+              <li className="links" key={index}>
+                <a href="/">{destination.name}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="mask" />
+      </>
     );
   }
 };
